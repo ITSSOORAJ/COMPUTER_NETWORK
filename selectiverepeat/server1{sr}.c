f42625fc718b4ab1e6e175325ee5c95260c6fc71
SOORAJ KUMAR <121786021+ITSSOORAJ@users.noreply.github.com>
2024-07-06 19:19:17 +0530

    Updated server1{sr}.c

--- selectiverepeat/server1{sr}.c
+++ selectiverepeat/server1{sr}.c
@@ -1,131 +1,143 @@
 #include<stdio.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
 #include<string.h>
 #include<time.h>
 #include<stdlib.h>
-#include<ctype.h>
+#include<sys/socket.h>
+#include<sys/types.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #define W 5
-#define P1 50
-#define P2 10
-
-char a[10];
-char b[10];
-void alpha9(int);
-void alp(int);
-
-int main()
-{
-    struct sockaddr_in ser,cli;
-    int s,n,sock,i,j,c=1,f;
-    unsigned int s1;
-    s=socket(AF_INET,SOCK_STREAM,0);
-    ser.sin_family=AF_INET;
-    ser.sin_port=6500;
-    ser.sin_addr.s_addr=inet_addr("127.0.0.1");
-    bind(s,(struct sockaddr *) &ser, sizeof(ser));
-    listen(s,1);
-    n=sizeof(cli);
-    sock=accept(s,(struct sockaddr *)&cli, &n);
-    printf("\nTCP Connection Established.\n");
-    s1=(unsigned int) time(NULL);
-    srand(s1);
-    strcpy(b,"Time Out ");
-    recv(sock,a,sizeof(a),0);
-    f=atoi(a);
-    while(1)
-    {
-        for(i=0;i<W;i++)
-        {
-            recv(sock,a,sizeof(a),0);
-            if(strcmp(a,b)==0)
-            {
-                break;
-            }
-        }
-        i=0;
-        while(i<W)
-        {
-            L:
-                j=rand()%P1;
-            if(j<P2)
-            {
-                alp(c);
-                send(sock,b,sizeof(b),0);
-                goto L;
-            }
-            else
-            {
-                alpha9(c);
-                if(c<=f)
-                {
-                    printf("\nFrame %s Received ",a);
-                    send(sock,a,sizeof(a),0);
-                }
-                else
-                {
-                    break;
-                }
-                c++;
-            }
-            if(c>f)
-            {
-                break;
-            }
-            i++;
-        }
-        if(c>f)
-        {
-            break;
-        }
-    }
-    close(sock);
-    close(s);
-    return 0;
-}
-
-void alpha9(int z)
-{
-    int k,i=0,j,g;
-    k=z;
-    while(k>0)
-    {
-        i++;
-        k=k/10;
-    }
-    g=i;
-    i--;
-    while(z>0)
-    {
-        k=z%10;
-        a[i]=k+48;
-        i--;
-        z=z/10;
-    }
-    a[g]='\0';
-}
+#define p1 50
+#define p2 10
+#define port 6500
+char a[90];
+char b[90];
 
 void alp(int z)
 {
-    int k,i=1,j,g;
-    k=z;
-    b[0]='N';
-    while(k>0)
-    {
-        i++;
-        k=k/10;
-    }
-    g=i;
-    i--;
-    while(z>0)
-    {
-        k=z%10;
-        b[i]=k+48;
-        i--;
-        z=z/10;
-    }
-    b[g]='\0';
+ int k,i=1,g;
+         k=z;
+	 b[0]='N';
+         while(k>0)
+                {
+                 i++;
+                 k=k/10;
+                }
+                g=i;
+                i--;
+         while(z>0)
+                {
+                 k=z%10;
+                 b[i]=k+48;
+                 i--;
+                 z=z/10;
+                }
+                b[g]='\0';
+
+}
+
+void alpha(int z)
+{
+ int k,i=0,g;
+         k=z;
+         while(k>0)
+                {
+                 i++;
+                 k=k/10;
+                }
+                g=i;
+                i--;
+         while(z>0)
+                {
+                 k=z%10;
+                 a[i]=k+48;
+                 i--;
+                 z=z/10;
+                }
+                a[g]='\0';
+
+}
+
+
+int main()
+{
+int sockfd,confd,i=0,len,c=1,j,f;
+struct sockaddr_in servaddr,cliaddr;
+unsigned int sr;
+char v[90];
+printf("SELECTIVE REPEAT SERVER \n");
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+servaddr.sin_family=AF_INET;
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+servaddr.sin_port=htons(port);
+if(bind(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr))<0)
+	{
+	 printf("ERROR ON BINDING \n");
+	 exit(0);
+	}
+listen(sockfd,1);
+len=sizeof(cliaddr);
+confd=accept(sockfd,(struct sockaddr*)&cliaddr,&len);
+if(confd<0)
+	{
+	printf("ERROR ON CONNECTION \n");
+	exit(0);
+	}
+printf("CONNECTION ESTABLISHED \n");
+sr=(unsigned int ) time(NULL);
+srand(sr);
+strcpy(v,"TIMEOUT");
+recv(confd,a,sizeof(a),0);
+ f = atoi(a);
+printf("FRAME NUMBER IS %d\n",f);
+//fflush(stdout);
+while(1)
+	{
+	 for(i=0;i<W;i++)
+		{
+		 recv(confd,a,sizeof(a),0);
+		  if(strcmp(a,v)==0)
+			{
+			 printf("SESSION ENDED \n");
+			 exit(0);
+			}
+		}
+	i=0;
+	 while(i<W)	
+		{
+		 L: j =rand()%p1;
+		 if(j<p2)
+			{
+			  alp(c);
+			  send(confd,b,sizeof(b),0);
+			   goto L;	
+			}
+		  else
+			{
+			 alpha(c);
+			 if(c<=f)
+				{
+				 printf("FRAME %s RECIEVED\n",a);
+				 send(confd,a,sizeof(a),0);
+				c++;
+				}
+			  else
+				{
+				 break;
+				}
+			}
+			if(c>f)
+			{
+			 break;
+			}
+			i++;
+		}
+	/*if(c>f)
+		{
+		 break;
+		}*/
+	}
+close(confd);
+close(sockfd);
+return 0;
 }
