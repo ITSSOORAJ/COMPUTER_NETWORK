941aff5928ec45088b12ff66717a49e56d73b604
SOORAJ KUMAR <121786021+ITSSOORAJ@users.noreply.github.com>
2024-07-06 22:08:00 +0530

    Updated server.c

--- ftp/server.c
+++ ftp/server.c
@@ -1,43 +1,38 @@
 #include<stdio.h>
-#include<arpa/inet.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<stdlib.h>
 #include<string.h>
+#include<stdlib.h>
 #include<unistd.h>
-#define SERV_TCP_PORT 5035
-#define MAX 60
-int i, j, tem;
-char buff[4096], t;
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include<netinet/in.h>
+#define port 5035
+#define max 60
 FILE *f1;
-int main(int afg, char *argv)
+int  main()
 {
-       int sockfd, newsockfd, clength;
-       struct sockaddr_in serv_addr,cli_addr;
-       char t[MAX], str[MAX];
-       strcpy(t,"exit");
-       sockfd=socket(AF_INET, SOCK_STREAM,0);
-       serv_addr.sin_family=AF_INET;
-       serv_addr.sin_addr.s_addr=INADDR_ANY;
-       serv_addr.sin_port=htons(SERV_TCP_PORT);
-       //printf("\nBinded");
-       bind(sockfd,(struct sockaddr*)&serv_addr, sizeof(serv_addr));
-       //printf("\nListening...");
-       listen(sockfd, 5);
-       clength=sizeof(cli_addr);
-       newsockfd=accept(sockfd,(struct sockaddr*) &cli_addr,&clength);
-       close(sockfd);
-       read(newsockfd, &str, MAX);
-       printf("\nClient message\n File Name : %s\n", str);
-       f1=fopen(str, "r");
-       while(fgets(buff, 4096, f1)!=NULL)
-       {
-            write(newsockfd, buff,MAX);
-            printf("\n");
-       }
-       fclose(f1);
-       printf("\nFile Transfer succcesful\n");
-       return 0;
+int sockfd,confd,n;
+struct sockaddr_in servaddr, cliaddr;
+char send[max],recv[max];
+sockfd=socket(AF_INET,SOCK_STREAM,0);
+servaddr.sin_family=AF_INET;
+servaddr.sin_port=htons(port);
+servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+bind(sockfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
+listen(sockfd,5);
+int len=sizeof(cliaddr);
+confd=accept(sockfd,(struct sockaddr*)&cliaddr,&len);
+read(confd,recv,max);
+printf("FILENAME IS %s\n",recv);
+f1=fopen(recv,"r");
+while(fgets(send,max,f1)!=NULL)
+{
+write(confd,send,max);
+printf("\n");
+}
+fclose(f1);
+printf("FILE SUCCESFULLY TRANSFERRED\n");
+close(confd);
+close(sockfd);
+return 0;
 }
